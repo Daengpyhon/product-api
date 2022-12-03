6e5e860494fdd5b2b0f807525c0449ed2731ed6a
Daeng <91820737+Daengpyhon@users.noreply.github.com>
2022-12-03 09:44:42 +0700

    Update route.js

--- route.js
+++ route.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const route = express.Router();
 const {listProducts} = require('./controller')
-route.get('/', listProducts)
+route.get('/api', listProducts)
+route.get('/', (req, res)=>{
+  res.send("Welcome")
+})
 
 module.exports = route;
