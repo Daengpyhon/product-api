07de32d8e4574e35930a1f32cfc2d562b5da4345
Daeng <91820737+Daengpyhon@users.noreply.github.com>
2022-12-05 13:59:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
 
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
 const mongoDB = "mongodb+srv://mernapp:99199097@cluster0.dyrlanl.mongodb.net/e_com_db?retryWrites=true&w=majority";
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-
-app.use(express.json())
-app.use(cors({origin: '*'}))
-app.use(morgan('dev'))
-app.use(bodyParser.json({limit: '20mb'}))
 
 const route = require('./route')
 app.use(route)
